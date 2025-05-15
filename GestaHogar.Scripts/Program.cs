d84cdbe1ef1deff548ca8e73998a704e7c1ff24c
Adrián Estévez Cerqueira <aestevez@novossistemas.com>
2025-05-15 12:31:09 +0200

    Proyecto de scripts para añadir el evento a la base de datos añadido

---
+++ GestaHogar.Scripts/Program.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+MySqlConnection connection = new(
+    "Server=localhost;Database=GestaHogarDb;User=root;Password=;"
+);
+try
+{
+    connection.Open();
+    Console.WriteLine("Connection to the database opened");
+
+    MySqlCommand command = new(
+        "CREATE IF NOT EXISTS EVENT `update_stock` ON SCHEDULE EVERY 1 DAY DO UPDATE UserProduct SET CurrentStock = IF (CurrentStock < DailyUse,0, CurrentStock - DailyUse ); SET GLOBAL event_scheduler = ON;",
+        connection
+    );
+
+    command.ExecuteNonQuery();
+    Console.WriteLine("Event added successfully");
+}
+catch (MySqlException ex)
+{
+    Console.WriteLine(ex);
+}
+finally
+{
+    connection.Close();
+}
